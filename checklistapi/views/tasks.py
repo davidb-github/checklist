8a5f9b70bbf837fb66b0a5b1a0457e4a5d420e98
David Bing <davidb.devops@gmail.com>
2021-03-17 11:24:02 -0500

    update views/task.py: implement update method

--- checklistapi/views/tasks.py
+++ checklistapi/views/tasks.py
@@ -7,53 +7,50 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from rest_framework import status
-from datetime import date
 from checklistapi.models import Task, MyUser
 
 
 class Tasks(ViewSet):
+
     #Handle GET requests to get all tasks Returns: Response:JSON serialized list of all tasks
     def list(self, request):
         # Get all task records from database
-        all_tasks = Task.objects.all()
-
+        all_tasks    = Task.objects.all()
         # Get current logged in user
         current_user = MyUser.objects.get(user=request.auth.user)
-
         # Get all tasks associated to the current_user
-        all_tasks = all_tasks.filter(user=current_user)
+        all_tasks    = all_tasks.filter(user=current_user)
 
         # Note the additional `many=True` argument to the
         # serializer. It's needed when you are serializing
         # a list of objects instead of a single object.
         serializer = TaskSerializer(
             all_tasks, many=True, context={'request': request})
-            
+
         return Response(serializer.data)
 
     #Handle GET requests for single task Returns:Response -- JSON serialized task
     def retrieve(self, request, pk=None):
-
         try:
-            task = Task.objects.get(pk=pk)
+            task       = Task.objects.get(pk=pk)
             serializer = TaskSerializer(
                 task, context={'request': request})
             return Response(serializer.data)
 
         except Exception as ex:
             return HttpResponseServerError(ex)
-    
+
+
     #Handle POST operations for tasks Returns: Response -- JSON serialized task instance
     def create(self, request):
-
         current_user = MyUser.objects.get(user=request.auth.user)
 
         # Grab data from client's request to build a new task instance
-        task = Task()
-        task.user = current_user
-        task.task_name = request.data["task_name"]
+        task                  = Task()
+        task.user             = current_user
+        task.task_name        = request.data["task_name"]
         task.task_description = request.data["task_description"]
-        task.is_complete = False
+        task.is_complete      = False
 
         # Try to save the new game to the database, then
         # serialize the game instance as JSON, and send the
@@ -68,6 +65,27 @@ class Tasks(ViewSet):
         # client that something was wrong with its request data
         except ValidationError as ex:
             return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
+
+
+    #Handle PUT requests for a task Returns:Response -- Empty body with 204 status code
+    def update(self, request, pk=None):
+        current_user = MyUser.objects.get(user=request.auth.user)
+
+        # Do mostly the same thing as POST, but instead of
+        # creating a new instance of Task, get the task record
+        # from the database whose primary key is `pk`
+        task                  = Task.objects.get(pk=pk)
+        task.user             = current_user
+        task.task_name        = request.data["task_name"]
+        task.task_description = request.data["task_description"]
+        task.is_complete      = request.data["is_complete"]
+
+        # Save the updated task instance to database,
+        # overwriting the original values.
+        task.save()
+
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
+
 
 ## Serializers ##
 
